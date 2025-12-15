2ad94bc7ac24f645fcefddf867484fe023e4300d
mariachiaravoltaggio <contrader.int@pcm637.onmicrosoft.com>
2025-12-15 11:56:58 +0100

    fix(setup): version 19

--- apps/showcase/server/main.server.ts
+++ apps/showcase/server/main.server.ts
@@ -2,6 +2,11 @@ import { AppComponent } from '@/components/layout/app.component';
 import { config } from '@/server/app.config.server';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => {
+    return bootstrapApplication(AppComponent, config).catch((err) => {
+        console.error('Bootstrap error:', err);
+        throw err;
+    });
+};
 
 export default bootstrap;
